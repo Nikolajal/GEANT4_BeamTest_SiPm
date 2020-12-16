a77aedae78c52ad074cfbc2b44b0a700f4f6b10c
Nicola rubini <nrubini@pop-os.localdomain>
2020-12-16 09:55:55 +0100

    Name change

---
+++ B1-build/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1/exampleB1.cc" "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1-build/CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1/src/B1ActionInitialization.cc" "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1-build/CMakeFiles/exampleB1.dir/src/B1ActionInitialization.cc.o"
+  "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1/src/B1DetectorConstruction.cc" "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1-build/CMakeFiles/exampleB1.dir/src/B1DetectorConstruction.cc.o"
+  "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1/src/B1EventAction.cc" "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1-build/CMakeFiles/exampleB1.dir/src/B1EventAction.cc.o"
+  "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1/src/B1PrimaryGeneratorAction.cc" "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1-build/CMakeFiles/exampleB1.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1/src/B1RunAction.cc" "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1-build/CMakeFiles/exampleB1.dir/src/B1RunAction.cc.o"
+  "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1/src/B1SteppingAction.cc" "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1-build/CMakeFiles/exampleB1.dir/src/B1SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/nrubini/alidock/GEANT4_BeamTest_SiPm/B1/include"
+  "/home/nrubini/Applications/Geant4_10.07/geant4.10.07-install/include/Geant4"
+  "/home/nrubini/Applications/Geant4_10.07/geant4.10.07-install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
