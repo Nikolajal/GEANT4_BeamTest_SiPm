7e39dac79b6362d9e7ab77ffb9f54b3eed26f603
Nicola Rubini <nicola.rubini@cern.ch>
2021-02-02 16:43:29 +0100

    Typo

--- B1/exampleB1.cc
+++ B1/exampleB1.cc
@@ -24,64 +24,64 @@
 
 int main(int argc,char** argv)
 {
-  // Detect interactive mode (if no arguments) and define UI session
-  //
-  G4UIExecutive*    fUserInterface =  new G4UIExecutive(argc, argv, "tsch");
+      // Detect interactive mode (if no arguments) and define UI session
+      //
+      G4UIExecutive*    fUserInterface =  new G4UIExecutive(argc, argv, "tsch");
 
-  // Optionally: choose a different Random engine...
-  // G4Random::setTheEngine(new CLHEP::MTwistEngine);
-  
-  // Construct the default run manager
-  //
-#ifdef G4MULTITHREADED
-  G4MTRunManager* runManager = new G4MTRunManager;
-#else
-  G4RunManager* runManager = new G4RunManager;
-#endif
+      // Optionally: choose a different Random engine...
+      // G4Random::setTheEngine(new CLHEP::MTwistEngine);
+      
+      // Construct the default run manager
+      //
+    #ifdef G4MULTITHREADED
+      G4MTRunManager* runManager = new G4MTRunManager;
+    #else
+      G4RunManager* runManager = new G4RunManager;
+    #endif
 
-  // Set mandatory initialization classes
-  //
-  // Detector construction
-  runManager->SetUserInitialization(new DetectorConstruction());
+      // Set mandatory initialization classes
+      //
+      // Detector construction
+      runManager->SetUserInitialization(new DetectorConstruction());
 
-  // Physics list
-  runManager->SetUserInitialization(new QGSP_BERT_HP);
-    
-  // User action initialization
-  runManager->SetUserInitialization(new ActionInitialization());
-  
-  // Initialize visualization
-  //
-  G4VisManager* visManager = new G4VisExecutive;
-  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
-  // G4VisManager* visManager = new G4VisExecutive("Quiet");
-  visManager->Initialize();
+      // Physics list
+      runManager->SetUserInitialization(new QGSP_BERT_HP);
+        
+      // User action initialization
+      runManager->SetUserInitialization(new ActionInitialization());
+      
+      // Initialize visualization
+      //
+      G4VisManager* visManager = new G4VisExecutive;
+      // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+      // G4VisManager* visManager = new G4VisExecutive("Quiet");
+      visManager->Initialize();
 
-  // Get the pointer to the User Interface manager
-  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+      // Get the pointer to the User Interface manager
+      G4UImanager* UImanager = G4UImanager::GetUIpointer();
 
-  // Process macro or start UI session
-  //
-  if ( argc != 1 ) {
-    // batch mode
-    G4String command = "/control/execute ";
-    G4String fileName = argv[1];
-    UImanager->ApplyCommand(command+fileName);
-  }
-  else {
-    // interactive mode
-    fUserInterface =  new G4UIExecutive(argc, argv, "Qt");
-    ui->SessionStart();
-    delete ui;
-  }
+      // Process macro or start UI session
+      //
+      if ( argc != 1 ) {
+        // batch mode
+        G4String command = "/control/execute ";
+        G4String fileName = argv[1];
+        UImanager->ApplyCommand(command+fileName);
+      }
+      else {
+        // interactive mode
+        fUserInterface =  new G4UIExecutive(argc, argv, "Qt");
+        fUserInterface->SessionStart();
+      }
 
-  // Job termination
-  // Free the store: user actions, physics_list and detector_description are
-  // owned and deleted by the run manager, so they should not be deleted
-  // in the main() program !
-  
-  delete visManager;
-  delete runManager;
+      // Job termination
+      // Free the store: user actions, physics_list and detector_description are
+      // owned and deleted by the run manager, so they should not be deleted
+      // in the main() program !
+        
+      delete fUserInterface;
+      delete visManager;
+      delete runManager;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
