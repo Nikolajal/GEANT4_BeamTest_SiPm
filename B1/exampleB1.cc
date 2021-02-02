55fb4b4f8f86ec06202d8caa4c24a20390b4b64e
Nicola Rubini <nicola.rubini@cern.ch>
2021-02-02 16:42:41 +0100

    Change main

--- B1/exampleB1.cc
+++ B1/exampleB1.cc
@@ -26,10 +26,7 @@ int main(int argc,char** argv)
 {
   // Detect interactive mode (if no arguments) and define UI session
   //
-  G4UIExecutive* ui = 0;
-  if ( argc == 1 ) {
-    ui = new G4UIExecutive(argc, argv);
-  }
+  G4UIExecutive*    fUserInterface =  new G4UIExecutive(argc, argv, "tsch");
 
   // Optionally: choose a different Random engine...
   // G4Random::setTheEngine(new CLHEP::MTwistEngine);
@@ -48,9 +45,7 @@ int main(int argc,char** argv)
   runManager->SetUserInitialization(new DetectorConstruction());
 
   // Physics list
-  G4VModularPhysicsList* physicsList = new QGSP_BERT_HP;
-  physicsList->SetVerboseLevel(1);
-  runManager->SetUserInitialization(physicsList);
+  runManager->SetUserInitialization(new QGSP_BERT_HP);
     
   // User action initialization
   runManager->SetUserInitialization(new ActionInitialization());
@@ -67,22 +62,22 @@ int main(int argc,char** argv)
 
   // Process macro or start UI session
   //
-  if ( ! ui ) { 
+  if ( argc != 1 ) {
     // batch mode
     G4String command = "/control/execute ";
     G4String fileName = argv[1];
     UImanager->ApplyCommand(command+fileName);
   }
-  else { 
+  else {
     // interactive mode
-    UImanager->ApplyCommand("/control/execute init_vis.mac");
+    fUserInterface =  new G4UIExecutive(argc, argv, "Qt");
     ui->SessionStart();
     delete ui;
   }
 
   // Job termination
   // Free the store: user actions, physics_list and detector_description are
-  // owned and deleted by the run manager, so they should not be deleted 
+  // owned and deleted by the run manager, so they should not be deleted
   // in the main() program !
   
   delete visManager;
